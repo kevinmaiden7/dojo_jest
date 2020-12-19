4327de57d6256cd6eccc25de9b2e7599f701edba
kevinmaiden7 <kevin.m.gallego@gmail.com>
2020-12-19 09:26:38 -0500

    setting up jest and performing some tests

---
+++ calculator.test.js
@@ -0,0 +1,29 @@
+// AAA Pattern
+
+const cal = require('./calculator'); // Arrange
+
+describe('add Operator', () =>{
+
+    test('adds 1 + 1 equals 2', () =>{
+        var value = cal.add(1,1); // Act
+        expect(value).toBe(2); // Assert
+    });
+    
+    test('adds 5 + 2 equals 7', () =>{
+        var value = cal.add(5,2);
+        expect(value).toBe(7);
+    });
+});
+
+describe('subtract Operator', () =>{
+    
+    test('subtracts 2 - 1 equals 1', () =>{
+        var value = cal.subtract(2,1);
+        expect(value).toBe(1);
+    });
+    
+    test('adds 5 - 2 equals 3', () =>{
+        var value = cal.subtract(5,2);
+        expect(value).toBe(3);
+    });
+});
