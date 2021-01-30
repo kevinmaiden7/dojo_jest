ee2bf79bff4636684e0fb7fb6fa2223b7fd4bcd9
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-01-30 09:11:09 -0500

    Fixing port mapping

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-var port = process.env.PORT | 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("The app is now running on port", port);
