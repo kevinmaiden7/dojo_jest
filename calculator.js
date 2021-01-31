4bfd42f8873e7ae16880e74c5ce4d60cae031ab4
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-01-31 10:21:14 -0500

    Altering mult function (testing purposes)

--- calculator.js
+++ calculator.js
@@ -7,8 +7,9 @@ function subtract(a, b){
 }
 
 function  mult(a, b){
-    return a * b;
+    return a / b;
 }
 
 module.exports.add = add;
 module.exports.subtract = subtract;
+module.exports.mult = mult;
