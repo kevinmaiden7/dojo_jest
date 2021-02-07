69bd0e51b8ddf1c3c80e1372e6352be01edcd40d
Kevin Martínez Gallego <kevin.m.gallego@gmail.com>
2021-02-06 23:39:03 -0500

    Develop (#7)
    * Adding mult function
    
    * Altering mult function (testing purposes)
    
    * Adding div operator (#2)
    
    * Adding code coverage info (#4)
    
    * Adding mult and div tests (#6)

--- api.calculator.test.js
+++ api.calculator.test.js
@@ -62,7 +62,7 @@ describe("testing /add path", () => {
     });
 });
 
-describe("testing /subtract path", () => {
+describe("testing /sub path", () => {
 
     test("it should return status code 200", done => {
         request(app)
@@ -97,6 +97,86 @@ describe("testing /subtract path", () => {
         .get("/sub?a=6&b=4")
         .then(response => {
             expect(response.body.result).toBe(2);
+            done();
+        });
+    });
+});
+
+describe("testing /mult path", () => {
+
+    test("it should return status code 200", done => {
+        request(app)
+        .get("/mult?a=1&b=1")
+        .then(response => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+
+    test("it should return application/json content-type", done => {
+        request(app)
+        .get("/mult?a=1&b=1")
+        .then(response => {
+            expect(response.type).toBe("application/json");
+            done();
+        });
+    });
+
+    test("it should return a valid json object", done => {
+        request(app)
+        .get("/mult?a=1&b=1")
+        .then(response => {
+            expect(response.body).not.toBeNull();
+            expect(response.body.result).toBeDefined();
+            done();
+        });
+    });
+
+    test("it should return a correct answer", done => {
+        request(app)
+        .get("/mult?a=6&b=4")
+        .then(response => {
+            expect(response.body.result).toBe(24);
+            done();
+        });
+    });
+});
+
+describe("testing /div path", () => {
+
+    test("it should return status code 200", done => {
+        request(app)
+        .get("/div?a=1&b=1")
+        .then(response => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+
+    test("it should return application/json content-type", done => {
+        request(app)
+        .get("/div?a=1&b=1")
+        .then(response => {
+            expect(response.type).toBe("application/json");
+            done();
+        });
+    });
+
+    test("it should return a valid json object", done => {
+        request(app)
+        .get("/div?a=1&b=1")
+        .then(response => {
+            expect(response.body).not.toBeNull();
+            expect(response.body.result).toBeDefined();
+            done();
+        });
+    });
+
+    test("it should return a correct answer", done => {
+        request(app)
+        .get("/div?a=6&b=4")
+        .then(response => {
+            expect(response.body.result).toBe(1.5);
             done();
         });
     });
